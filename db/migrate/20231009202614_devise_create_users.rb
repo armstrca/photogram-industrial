55c4743510e0a1ec906f9292ca83304432fa4499
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-09 20:33:20 +0000

    citext

--- db/migrate/20231009202614_devise_create_users.rb
+++ db/migrate/20231009202614_devise_create_users.rb
@@ -3,12 +3,13 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      enable_extension("citext")
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
@@ -32,17 +33,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
+      t.citext :username
       t.boolean :private
-      t.integer :likes_count
-      t.integer :comments_count
+      t.integer :likes_count, default: 0
+      t.integer :comments_count, default: 0
 
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+    add_index :users, :username, unique: true
   end
 end
