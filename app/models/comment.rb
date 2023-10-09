17c69ada9891a908b514950abc15316e6615395a
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-09 21:06:18 +0000

    Updated models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,11 @@
 #  photo_id   :integer
 #
 class Comment < ApplicationRecord
+  validates(:commenter, { :presence => true })
+
+
+  belongs_to(:commenter, :class_name => "User", :foreign_key => "author_id")
+
+
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
 end
--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -10,4 +10,13 @@
 #  sender_id    :integer
 #
 class FollowRequest < ApplicationRecord
+  validates(:sender, { :presence => true})
+  validates(:recipient, { :presence => true })
+  validates(:recipient_id, {
+    :uniqueness => { :scope => [:sender_id] }
+  })
+
+  belongs_to(:sender, :class_name => "User", :foreign_key => "sender_id")
+
+  belongs_to(:recipient, :class_name => "User", :foreign_key => "recipient_id")
 end
