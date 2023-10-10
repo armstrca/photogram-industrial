642e315d5500e88d22e3bfa8c2b1c86c79d8d5e8
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-10 15:32:29 +0000

    time to do a pull request

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,12 +41,12 @@ class User < ApplicationRecord
 
   has_many(:commented_photos, :through => "comments", :source => "photo")
 
-  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :sent_follow_requests, foreign_key: :sender_id, dependent: :destroy, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id,  dependent: :destroy, class_name: "FollowRequest"
 
-  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id,  dependent: :destroy, class_name: "FollowRequest"
 
-  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id,  dependent: :destroy, class_name: "FollowRequest"
 
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
