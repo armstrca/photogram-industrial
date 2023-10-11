ea696325d4086d31c77f0546e792930991ed2636
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-11 15:20:39 +0000

    fixed confusions with industrial-auth-1 changes that shouldn't have been here

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,16 @@ Rails.application.routes.draw do
   root "photos#index"
 
   devise_for :users
-
+  
   resources :comments
-  resources :follow_requests, except: [:index, :show, :new, :edit, :create]
-  resources :likes, only: [:create, :destroy]
-  resources :photos, except: [:index]
-
+  resources :follow_requests
+  resources :likes
+  resources :photos
 
   get ":username/liked" => "users#liked", as: :liked
-  get ":username/feed" => "users#feed", as: :feed
-  get ":username/followers" => "users#followers", as: :followers
-  get ":username/following" => "users#leaders", as: :leaders
+  get ":username/feed"
+  get ":username/followers"
+  get ":username/following"
+
   get ":username" => "users#show", as: :user
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
